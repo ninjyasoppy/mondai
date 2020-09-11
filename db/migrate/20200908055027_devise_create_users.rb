eacf100136a5853fd31062d41db9cccfc754e5f8
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-09-11 15:58:52 +0900

    ユーザー管理、編集権限

--- db/migrate/20200908055027_devise_create_users.rb
+++ db/migrate/20200908055027_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname, null: false
+      t.boolean :admin,              null: false, default: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_061412) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "car_name"
     t.string "price"
@@ -51,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_061412) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.boolean "admin", default: false, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
