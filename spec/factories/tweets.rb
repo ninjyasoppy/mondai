54cc918f573229f03c4f99a6bd310ddd8ba515e0
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-09-15 18:53:56 +0900

    factroyBot

---
+++ spec/factories/tweets.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :tweet do
+    car_name    {"スズキ ラパン"}
+    price       {"１０４.５"}
+    sales_point {"女性に人気車種"}
+    modele_year {"平成２９年４月"}
+    mileage     {"30000Km"}
+    inspection  {"2023年8月"}
+  end
+end
